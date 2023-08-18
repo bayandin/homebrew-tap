48c9bef98ee0f5adddb63ec4ce8e6d8eea3ca13f
Alexander Bayandin <alexander@bayandin.dev>
2023-08-18 15:53:27 +0000

    pg_hashids: add 1.2.1 bottle.

--- Formula/pg_hashids.rb
+++ Formula/pg_hashids.rb
@@ -5,6 +5,13 @@ class PgHashids < Formula
   sha256 "74576b992d9277c92196dd8d816baa2cc2d8046fe102f3dcd7f3c3febed6822a"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bayandin/tap"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2a991e54c25057f381e042385f91497ad2714294e83f2ba757e5d08357726dd6"
+    sha256 cellar: :any_skip_relocation, ventura:       "889a3223a5dce89ed4acf023d4f094f9de82b23c4f1a4c09075955442e606d39"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05de7f68135034e2ca61a1b62bc18349c828bb2d342e982bf38bdcd6960ba2c1"
+  end
+
   depends_on "bayandin/tap/neon-postgres"
 
   def neon_postgres
