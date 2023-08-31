356f59bf9b24a6a077d8600382d7b0780880c358
Alexander Bayandin <alexander@neon.tech>
2023-08-31 16:04:35 +0100

    neon-local 3819

--- Formula/neon-local.rb
+++ Formula/neon-local.rb
@@ -1,8 +1,8 @@
 class NeonLocal < Formula
   desc "CLI for running Neon locally"
   homepage "https://github.com/neondatabase/neon"
-  url "https://github.com/neondatabase/neon/archive/refs/tags/release-3808.tar.gz", using: :nounzip
-  sha256 "ecd6856c97dd1ae5d0b649f62f4d95b643597a08d28fcc6ba510526deff7fa12"
+  url "https://github.com/neondatabase/neon/archive/refs/tags/release-3819.tar.gz", using: :nounzip
+  sha256 "e139c6053c68753df3f41a5b70d85c1ca3ce3c6e81c8abb94a96b2a8e0275ea9"
   license "Apache-2.0"
 
   bottle do
