23f802052a5b011a8637ff2070c5d8b9d66e9661
Alexander Bayandin <alexander@neon.tech>
2023-09-13 12:49:31 +0100

    neon-storage 3916

--- Formula/neon-storage.rb
+++ Formula/neon-storage.rb
@@ -2,8 +2,8 @@ class NeonStorage < Formula
   desc "Storage components for Neon"
   homepage "https://github.com/neondatabase/neon"
   url "https://github.com/neondatabase/neon.git",
-    tag:      "release-3898",
-    revision: "0e6fdc8a587aacbc598afb9232eb81484ed3494e"
+    tag:      "release-3916",
+    revision: "dce91b33a4ce24b1526ef1c39a95761cb0d7da2b"
   license "Apache-2.0"
   head "https://github.com/neondatabase/neon.git", branch: "main"
 
