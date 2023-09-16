779d91335ffd3f4eff70cc49a4d8380a3ddf0485
Alexander Bayandin <alexander@neon.tech>
2023-09-16 17:06:16 +0100

    pgrx 0.10.1
    Closes #43.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>

--- Formula/pgrx.rb
+++ Formula/pgrx.rb
@@ -1,8 +1,8 @@
 class Pgrx < Formula
   desc "Build Postgres Extensions with Rust"
   homepage "https://github.com/pgcentralfoundation/pgrx"
-  url "https://github.com/pgcentralfoundation/pgrx/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "0e81776fadc4c21f6e7dff95f69a8d23d292da22c87e35bab4ae9edd15e4e686"
+  url "https://github.com/pgcentralfoundation/pgrx/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "b93b3e75dd7484c14e383ccb7f026d6b6cbec584fdb476c9c4f670a9e745ae50"
   license "MIT"
 
   bottle do
@@ -12,7 +12,7 @@ class Pgrx < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "6313a3a8e545255d4f312163461124e8d3701a3a16a55148422fb183132a857d"
   end
 
-  depends_on "postgresql@15" => :test
+  depends_on "postgresql@16" => :test
   depends_on "rust"
   depends_on "rustfmt"
 
@@ -21,13 +21,13 @@ class Pgrx < Formula
   end
 
   test do
-    system "cargo", "pgrx", "init", "--pg15", Formula["postgresql@15"].opt_bin/"pg_config"
+    system "cargo", "pgrx", "init", "--pg16", Formula["postgresql@16"].opt_bin/"pg_config"
     system "cargo", "pgrx", "new", "example"
     cd "example" do
       # Postgres symbols won't be available until runtime
       ENV["RUSTFLAGS"] = "-Clink-arg=-Wl,-undefined,dynamic_lookup"
 
-      system "cargo", "pgrx", "package", "--pg-config", Formula["postgresql@15"].opt_bin/"pg_config",
+      system "cargo", "pgrx", "package", "--pg-config", Formula["postgresql@16"].opt_bin/"pg_config",
                                          "--out-dir", testpath/"out"
     end
   end
