52fb2c4f6919affa63e66dc96cc3d19965d5e00d
Alexander Bayandin <alexander@neon.tech>
2023-09-17 19:34:24 +0100

    pgrx: fix Postgres 16 on macOS
    Closes #45.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>

--- Formula/pgrx.rb
+++ Formula/pgrx.rb
@@ -4,6 +4,7 @@ class Pgrx < Formula
   url "https://github.com/pgcentralfoundation/pgrx/archive/refs/tags/v0.10.1.tar.gz"
   sha256 "b93b3e75dd7484c14e383ccb7f026d6b6cbec584fdb476c9c4f670a9e745ae50"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
@@ -15,6 +16,12 @@ class Pgrx < Formula
   depends_on "postgresql@16" => :test
   depends_on "rust"
   depends_on "rustfmt"
+
+  # Fix for Postgres 16 on macOS
+  patch do
+    url "https://gist.githubusercontent.com/bayandin/f89ddb6af47ac994a325ba856c68e7d1/raw/0aa70e53afb60391bbb9e8fb7c1a30887a7df8bf/0001-Fix-install-for-Postgres-16-on-macOS.patch"
+    sha256 "86ca94cdef8dd3e1211eabb5f104787bc166b4dc3ce5c6f42a454a1d0fac9613"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args(path: "cargo-pgrx")
