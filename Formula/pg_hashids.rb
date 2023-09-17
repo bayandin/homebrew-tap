4a35231e20655dfa7bfcb11636464ab4dbeb059d
Alexander Bayandin <alexander@neon.tech>
2023-09-17 11:05:24 +0100

    pg_hashids: revision bump for Postgres 16

--- Formula/pg_hashids.rb
+++ Formula/pg_hashids.rb
@@ -4,6 +4,7 @@ class PgHashids < Formula
   url "https://github.com/iCyberon/pg_hashids/archive/refs/tags/v1.2.1.tar.gz"
   sha256 "74576b992d9277c92196dd8d816baa2cc2d8046fe102f3dcd7f3c3febed6822a"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
@@ -18,13 +19,20 @@ class PgHashids < Formula
     Formula["bayandin/tap/neon-postgres"]
   end
 
+  def pg_versions
+    neon_postgres.pg_versions with: "v16"
+  end
+
   def install
-    neon_postgres.pg_versions.each do |v|
+    pg_versions.each do |v|
+      # Ref https://github.com/postgres/postgres/commit/b55f62abb2c2e07dfae99e19a2b3d7ca9e58dc1a
+      dlsuffix = (OS.linux? || "v14 v15".include?(v)) ? "so" : "dylib"
+
       system "make", "clean", "PG_CONFIG=#{neon_postgres.pg_bin_for(v)}/pg_config"
       system "make", "PG_CONFIG=#{neon_postgres.pg_bin_for(v)}/pg_config"
 
       mkdir_p lib/neon_postgres.name/v
-      mv "pg_hashids.so", lib/neon_postgres.name/v
+      mv "pg_hashids.#{dlsuffix}", lib/neon_postgres.name/v
 
       mkdir_p share/neon_postgres.name/v/"extension"
       cp "pg_hashids.control", share/neon_postgres.name/v/"extension"
@@ -33,7 +41,7 @@ class PgHashids < Formula
   end
 
   test do
-    neon_postgres.pg_versions.each do |v|
+    pg_versions.each do |v|
       pg_ctl = neon_postgres.pg_bin_for(v)/"pg_ctl"
       psql = neon_postgres.pg_bin_for(v)/"psql"
       port = free_port
