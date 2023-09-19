7e61aa7398947bc055bc4f0316ca8f02d77e603d
Alexander Bayandin <alexander@neon.tech>
2023-09-19 23:23:04 +0100

    pgrx 0.10.2
    Closes #47.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>

--- Formula/pgrx.rb
+++ Formula/pgrx.rb
@@ -1,10 +1,9 @@
 class Pgrx < Formula
   desc "Build Postgres Extensions with Rust"
   homepage "https://github.com/pgcentralfoundation/pgrx"
-  url "https://github.com/pgcentralfoundation/pgrx/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "b93b3e75dd7484c14e383ccb7f026d6b6cbec584fdb476c9c4f670a9e745ae50"
+  url "https://github.com/pgcentralfoundation/pgrx/archive/refs/tags/v0.10.2.tar.gz"
+  sha256 "040fd7195fc350ec7c823e7c2dcafad2cf621c8696fd2ce0db7626d7fbd3d877"
   license "MIT"
-  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
@@ -16,12 +15,6 @@ class Pgrx < Formula
   depends_on "postgresql@16" => :test
   depends_on "rust"
   depends_on "rustfmt"
-
-  # Fix for Postgres 16 on macOS
-  patch do
-    url "https://gist.githubusercontent.com/bayandin/f89ddb6af47ac994a325ba856c68e7d1/raw/0aa70e53afb60391bbb9e8fb7c1a30887a7df8bf/0001-Fix-install-for-Postgres-16-on-macOS.patch"
-    sha256 "86ca94cdef8dd3e1211eabb5f104787bc166b4dc3ce5c6f42a454a1d0fac9613"
-  end
 
   def install
     system "cargo", "install", *std_cargo_args(path: "cargo-pgrx")
