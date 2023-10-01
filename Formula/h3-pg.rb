1711289b8a2062ed6ccf5a3f16ac2a21486d9341
Alexander Bayandin <alexander@bayandin.dev>
2023-10-01 01:46:34 +0100

    h3-pg: revision bump for Postgres 16

--- Formula/h3-pg.rb
+++ Formula/h3-pg.rb
@@ -4,6 +4,7 @@ class H3Pg < Formula
   url "https://github.com/zachasme/h3-pg/archive/refs/tags/v4.1.3.tar.gz"
   sha256 "5c17f09a820859ffe949f847bebf1be98511fb8f1bd86f94932512c00479e324"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
@@ -14,14 +15,22 @@ class H3Pg < Formula
 
   depends_on "cmake" => :build
   depends_on "bayandin/tap/neon-postgres"
-  depends_on "h3"
+
+  patch :DATA
 
   def neon_postgres
     Formula["bayandin/tap/neon-postgres"]
   end
 
+  def pg_versions
+    neon_postgres.pg_versions with: "v16"
+  end
+
   def install
-    neon_postgres.pg_versions.each do |v|
+    pg_versions.each do |v|
+      # Ref https://github.com/postgres/postgres/commit/b55f62abb2c2e07dfae99e19a2b3d7ca9e58dc1a
+      dlsuffix = (OS.linux? || "v14 v15".include?(v)) ? "so" : "dylib"
+
       mkdir buildpath/"build-#{v}"
 
       cd buildpath/"build-#{v}" do
@@ -32,7 +41,7 @@ class H3Pg < Formula
 
       stage_dir = Pathname("stage-#{v}#{HOMEBREW_PREFIX}")
       mkdir_p lib/neon_postgres.name/v
-      mv Dir[stage_dir/"lib"/neon_postgres.name/v/"*.so"], lib/neon_postgres.name/v
+      mv Dir[stage_dir/"lib"/neon_postgres.name/v/"*.#{dlsuffix}"], lib/neon_postgres.name/v
 
       from_ext_dir = stage_dir/"share"/neon_postgres.name/v/"extension"
       to_ext_dir = share/neon_postgres.name/v/"extension"
@@ -44,7 +53,7 @@ class H3Pg < Formula
   end
 
   test do
-    neon_postgres.pg_versions.each do |v|
+    pg_versions.each do |v|
       pg_ctl = neon_postgres.pg_bin_for(v)/"pg_ctl"
       psql = neon_postgres.pg_bin_for(v)/"psql"
       port = free_port
@@ -65,3 +74,22 @@ class H3Pg < Formula
     end
   end
 end
+
+__END__
+diff --git a/cmake/AddPostgreSQLExtension.cmake b/cmake/AddPostgreSQLExtension.cmake
+index 655cbc2..e316ad3 100644
+--- a/cmake/AddPostgreSQLExtension.cmake
++++ b/cmake/AddPostgreSQLExtension.cmake
+@@ -44,6 +44,12 @@ function(PostgreSQL_add_extension LIBRARY_NAME)
+       PREFIX "" # Avoid lib* prefix on output file
+     )
+
++    # Since Postgres 16, the shared library extension on macOS is `dylib`, not `so`.
++    # Ref https://github.com/postgres/postgres/commit/b55f62abb2c2e07dfae99e19a2b3d7ca9e58dc1a
++    if (APPLE AND ${PostgreSQL_VERSION_MAJOR} VERSION_GREATER_EQUAL "16")
++      set_target_properties (${LIBRARY_NAME} PROPERTIES SUFFIX ".dylib")
++    endif()
++
+     # Install .so/.dll to pkglib-dir
+     install(
+       TARGETS ${LIBRARY_NAME}
