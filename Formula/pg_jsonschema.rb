cb96d7072509c2cfb7cfa59138b079494cca55f6
Alexander Bayandin <alexander@neon.tech>
2023-10-02 18:28:39 +0100

    pg_jsonschema 0.2.0 (new formula)
    Closes #59.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>

---
+++ Formula/pg_jsonschema.rb
@@ -0,0 +1,87 @@
+class PgJsonschema < Formula
+  desc "PostgreSQL extension providing JSON Schema validation"
+  homepage "https://github.com/supabase/pg_jsonschema"
+  url "https://github.com/supabase/pg_jsonschema/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "b1bd95009c8809bd6cda9a37777f8b7df425ff1a34976c1e7a4b31cf838ace66"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+  depends_on "rustfmt" => :build
+  depends_on "bayandin/tap/neon-postgres"
+
+  uses_from_macos "llvm" => :build
+
+  resource "pgrx" do
+    url "https://github.com/pgcentralfoundation/pgrx/archive/refs/tags/v0.10.2.tar.gz"
+    sha256 "040fd7195fc350ec7c823e7c2dcafad2cf621c8696fd2ce0db7626d7fbd3d877"
+  end
+
+  def neon_postgres
+    Formula["bayandin/tap/neon-postgres"]
+  end
+
+  def pg_versions
+    neon_postgres.pg_versions with: "v16"
+  end
+
+  def install
+    resource("pgrx").stage do
+      system "cargo", "install", *std_cargo_args(root: buildpath/"pgrx", path: "cargo-pgrx")
+      ENV.prepend_path "PATH", buildpath/"pgrx/bin"
+    end
+
+    inreplace "Cargo.toml", /pgrx = "([^"]+)"/,
+                            "pgrx = { version = \"\\1\", features = [ \"unsafe-postgres\" ] }"
+
+    # Postgres symbols won't be available until runtime
+    ENV["RUSTFLAGS"] = "-Clink-arg=-Wl,-undefined,dynamic_lookup"
+
+    args = []
+    pg_versions.each do |v|
+      args << "--pg#{v.delete_prefix("v")}" << (neon_postgres.pg_bin_for(v)/"pg_config")
+    end
+    system "cargo", "pgrx", "init", *args
+
+    pg_versions.each do |v|
+      # Ref https://github.com/postgres/postgres/commit/b55f62abb2c2e07dfae99e19a2b3d7ca9e58dc1a
+      dlsuffix = (OS.linux? || "v14 v15".include?(v)) ? "so" : "dylib"
+
+      system "cargo", "pgrx", "package", "--profile", "release",
+                                        "--pg-config", neon_postgres.pg_bin_for(v)/"pg_config",
+                                        "--out-dir", "stage-#{v}"
+
+      stage_dir = Pathname("stage-#{v}#{HOMEBREW_PREFIX}")
+      mkdir_p lib/neon_postgres.name/v
+      mv stage_dir/"lib/neon-postgres/#{v}/pg_jsonschema.#{dlsuffix}", lib/neon_postgres.name/v
+
+      from_ext_dir = stage_dir/"share/neon-postgres/#{v}/extension"
+      to_ext_dir = share/neon_postgres.name/v/"extension"
+
+      mkdir_p to_ext_dir
+      mv from_ext_dir/"pg_jsonschema.control", to_ext_dir
+      mv Dir[from_ext_dir/"pg_jsonschema--*.sql"], to_ext_dir
+    end
+  end
+
+  test do
+    pg_versions.each do |v|
+      pg_ctl = neon_postgres.pg_bin_for(v)/"pg_ctl"
+      psql = neon_postgres.pg_bin_for(v)/"psql"
+      port = free_port
+
+      system pg_ctl, "initdb", "-D", testpath/"test-#{v}"
+      (testpath/"test-#{v}/postgresql.conf").write <<~EOS, mode: "a+"
+        port = #{port}
+      EOS
+      system pg_ctl, "start", "-D", testpath/"test-#{v}", "-l", testpath/"log-#{v}"
+      begin
+        system psql, "-p", port.to_s, "-c", <<~SQL, "postgres"
+          CREATE EXTENSION pg_jsonschema;
+          SELECT json_matches_schema('{"type": "object"}', '{}');
+        SQL
+      ensure
+        system pg_ctl, "stop", "-D", testpath/"test-#{v}"
+      end
+    end
+  end
+end
--- README.md
+++ README.md
@@ -43,6 +43,7 @@ In addition, you can try Neon locally with extentions from this tap. To install 
 - [`pg_embedding`](Formula/pg_embedding.rb) — HNSW algorithm for vector similarity search in PostgreSQL
 - [`pg_graphql`](Formula/pg_graphql.rb) — GraphQL support for PostgreSQL
 - [`pg_hashids`](Formula/pg_hashids.rb) — Short unique id generator for PostgreSQL, using hashids
+- [`pg_jsonschema`](Formula/pg_jsonschema.rb) — PostgreSQL extension providing JSON Schema validation
 - [`pg_tiktoken`](Formula/pg_tiktoken.rb) — Tiktoken tokenizer for PostgreSQL
 - [`pgjwt`](Formula/pgjwt.rb) — PostgreSQL implementation of JWT (JSON Web Tokens)
 - [`pgvector`](Formula/pgvector.rb) — Open-source vector similarity search for Postgres
