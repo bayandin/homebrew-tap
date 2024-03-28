a065e42ccee2cdc2ae4048aeac5a9b084c729527
Alexander Bayandin <alexander@bayandin.dev>
2024-03-28 15:38:15 +0000

    neon-proxy: add 5201 bottle.

--- Formula/neon-proxy.rb
+++ Formula/neon-proxy.rb
@@ -12,6 +12,13 @@ class NeonProxy < Formula
     regex(/^release-proxy-(\d+)$/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/bayandin/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "5e096e2c3a230887bf484933f064fc6b9439747f272195cb4591044b48840697"
+    sha256 cellar: :any_skip_relocation, ventura:      "483fd919778c4d80ee72acec1e326cb40e0bf7eb6a2f4aac67003144eb22459e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "04a2e46df785ae22a59c084fd648a356d7158efc055ce1c9c2f1492cf27a7ca0"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
