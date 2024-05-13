b39ab7ba0dffdbc0e809b0e0f2ab1196f1239c67
Alexander Bayandin <alexander@bayandin.dev>
2024-05-13 14:44:33 +0000

    pg_graphql: update 1.5.4 bottle.

--- Formula/pg_graphql.rb
+++ Formula/pg_graphql.rb
@@ -7,9 +7,9 @@ class PgGraphql < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
-    sha256 cellar: :any,                 arm64_sonoma: "d7674a0bad4217f28cdb41a50b2c02411fe6d7a5dc47011b5346270fdfdfa866"
-    sha256 cellar: :any,                 ventura:      "b04ca2d73d1c4e34de8fbf7074f6a79aa4e1f7100c83694a73fb08eabd8a65f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b66d9176c2b309c9033b5434a3a5b5924d17050d4e46061865a9a8ea30ca0dd5"
+    sha256 cellar: :any,                 arm64_sonoma: "948be55a920c94379276a2e5d0389e9f2343f02d0d406f304f1bb1e87e5f07d6"
+    sha256 cellar: :any,                 ventura:      "355aa62ec9f3ed7c61401fcae7f41f26831a22aa18dd72e7c7725f7cfb8cb8ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1fad69a1d8096d96bbe7d4f0901b6f976c352625d563e5243f1f5da3d9eb92f8"
   end
 
   depends_on "rust" => :build
