0ecedf2ce2af511ee040965871bf7831deefe518
Alexander Bayandin <alexander@bayandin.dev>
2024-06-23 20:53:29 +0000

    s5cmd: add 2.2.2 bottle.

--- Formula/s5cmd.rb
+++ Formula/s5cmd.rb
@@ -6,6 +6,13 @@ class S5cmd < Formula
   license "MIT"
   head "https://github.com/peak/s5cmd.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bayandin/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "7708aec8ff8297789970797998a29688dd491e73b7c1bd8c0e09ac4d6f6f00f1"
+    sha256 cellar: :any_skip_relocation, ventura:      "90c918923ab13f2b1116c7ba39f73887b2758103dde90a16fa51f4b382e618ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2a13f226103c575ad091b97ee3f9d82dfeb56461d0385dd03b1f009dfd1f3ca6"
+  end
+
   depends_on "go" => :build
 
   def install
