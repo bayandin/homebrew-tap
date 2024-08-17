0bf2831a840d0bd2ef3c47853d83c4d898e85c28
Alexander Bayandin <alexander@bayandin.dev>
2024-08-17 22:53:54 +0000

    neon-proxy: update 6158 bottle.

--- Formula/neon-proxy.rb
+++ Formula/neon-proxy.rb
@@ -14,9 +14,9 @@ class NeonProxy < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "aebaaa78696c4c25aa9bdf4c37df94ae6cae80feb54a4d7029b1d0af0baded6a"
-    sha256 cellar: :any_skip_relocation, ventura:      "79aa24161378c2b0228074be2bb3f5e37c23c9112074714d417d8d471068d5c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "eed326140635362c699e636ee3f836c6c6e6b5ac0b2ed69ad675d40aaad6305d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0f7ea3528e2ce7b5140f7dd6b7de6624aa796b4bdf55bc277211b345aad44eae"
+    sha256 cellar: :any_skip_relocation, ventura:      "9a92649e97f45e05a99839b47cdb2261274b0e00390376b605eb0e8dec296f5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "21c9f6e626b122b2767fc9b6cdbeb041df7dc5d1f2e2330d02b272ad61fb3900"
   end
 
   depends_on "rust" => :build
