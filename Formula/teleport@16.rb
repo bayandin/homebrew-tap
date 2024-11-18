905b5ab356dfa926adb53d230b1a29ca337a111f
Alexander Bayandin <alexander@neon.tech>
2024-11-18 15:48:02 +0000

    teleport@16 16.4.7 (new formula)
    Closes #196.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>

---
+++ Formula/teleport@16.rb
@@ -0,0 +1,79 @@
+class TeleportAT16 < Formula
+  desc "Modern SSH server for teams managing distributed infrastructure"
+  homepage "https://goteleport.com/"
+  url "https://github.com/gravitational/teleport/archive/refs/tags/v16.4.7.tar.gz"
+  sha256 "34dc8ab9065563efef29ef2e9de65f2c15f063fb69f712db15bb7fc46fd4edfa"
+  license all_of: ["AGPL-3.0-or-later", "Apache-2.0"]
+  head "https://github.com/gravitational/teleport.git", branch: "master"
+
+  # As of writing, two major versions of `teleport` are being maintained
+  # side by side and the "latest" release can point to an older major version,
+  # so we can't use the `GithubLatest` strategy. We use the `GithubReleases`
+  # strategy instead of `Git` because there is often a notable gap (days)
+  # between when a version is tagged and released.
+  livecheck do
+    url :stable
+    regex(/^v?(16(?:\.\d+)+)$/i)
+    strategy :github_releases
+  end
+
+  # Use "go" again after https://github.com/gravitational/teleport/commit/e4010172501f0ed18bb260655c83606dfa872fbd
+  # is released, likely in a version 17.x.x (or later?):
+  depends_on "go@1.22" => :build
+  depends_on "pkg-config" => :build
+  depends_on "pnpm" => :build
+  depends_on "rust" => :build
+  # TODO: try to remove rustup dependancy, see https://github.com/Homebrew/homebrew-core/pull/191633#discussion_r1774378671
+  depends_on "rustup" => :build
+  depends_on "wasm-pack" => :build
+  depends_on "libfido2"
+  depends_on "node"
+  depends_on "openssl@3"
+
+  uses_from_macos "curl" => :test
+  uses_from_macos "netcat" => :test
+  uses_from_macos "zip"
+
+  # FormulaAudit/Conflicts: Versioned formulae should not use conflicts_with. Use keg_only :versioned_formula instead.
+  # rubocop:disable FormulaAudit/Conflicts, Style/DisableCopsWithinSourceCodeDirective
+  conflicts_with "etsh", because: "both install `tsh` binaries"
+  conflicts_with "tctl", because: "both install `tctl` binaries"
+  # rubocop:enable FormulaAudit/Conflicts, Style/DisableCopsWithinSourceCodeDirective
+
+  def install
+    ENV.prepend_path "PATH", Formula["rustup"].bin
+    system "rustup", "default", "stable"
+    system "rustup", "set", "profile", "minimal"
+
+    ENV.deparallelize { system "make", "full", "FIDO2=dynamic" }
+    bin.install Dir["build/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/teleport version")
+    assert_match version.to_s, shell_output("#{bin}/tsh version")
+    assert_match version.to_s, shell_output("#{bin}/tctl version")
+
+    mkdir testpath/"data"
+    (testpath/"config.yml").write <<~EOS
+      version: v2
+      teleport:
+        nodename: testhost
+        data_dir: #{testpath}/data
+        log:
+          output: stderr
+          severity: WARN
+    EOS
+
+    fork do
+      exec "#{bin}/teleport start --roles=proxy,node,auth --config=#{testpath}/config.yml"
+    end
+
+    sleep 10
+    system "curl", "--insecure", "https://localhost:3080"
+
+    status = shell_output("#{bin}/tctl --config=#{testpath}/config.yml status")
+    assert_match(/Cluster\s*testhost/, status)
+    assert_match(/Version\s*#{version}/, status)
+  end
+end
