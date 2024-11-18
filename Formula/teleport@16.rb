06e8171b1b648c5301f0b82cda37bf2835dd844f
Alexander Bayandin <alexander@bayandin.dev>
2024-11-18 17:32:31 +0000

    teleport@16: add 16.4.7 bottle.

--- Formula/teleport@16.rb
+++ Formula/teleport@16.rb
@@ -17,6 +17,14 @@ class TeleportAT16 < Formula
     strategy :github_releases
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/bayandin/tap"
+    sha256 cellar: :any,                 arm64_sequoia: "b0dc5d7e774a1fb74f8c9e67cd9228d37c0214075670e9c52c90d1534e4e9ce5"
+    sha256 cellar: :any,                 arm64_sonoma:  "bc1d3bc61bac58cf2121a43e1ab536646e47f0e8ddf93c1b5ae082ab0da7f847"
+    sha256 cellar: :any,                 ventura:       "234f9439ca3b75d8ea66927b31759a4cee429770c7a700932633c9476cdbfbe4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf60784efdce7d94270b36c54f123c4524926f71adf6f4b550c436d62bd0c38e"
+  end
+
   # Use "go" again after https://github.com/gravitational/teleport/commit/e4010172501f0ed18bb260655c83606dfa872fbd
   # is released, likely in a version 17.x.x (or later?):
   depends_on "go@1.22" => :build
