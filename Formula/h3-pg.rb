5126eef3a5060672abc1ade8e5042f7cc7e03a2e
Alexander Bayandin <alexander@neon.tech>
2024-11-19 17:56:35 +0000

    h3-pg 4.1.4
    Closes #201.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>

--- Formula/h3-pg.rb
+++ Formula/h3-pg.rb
@@ -1,10 +1,9 @@
 class H3Pg < Formula
   desc "PostgreSQL bindings for H3, a hierarchical hexagonal geospatial indexing system"
   homepage "https://github.com/zachasme/h3-pg"
-  url "https://github.com/zachasme/h3-pg/archive/refs/tags/v4.1.3.tar.gz"
-  sha256 "5c17f09a820859ffe949f847bebf1be98511fb8f1bd86f94932512c00479e324"
+  url "https://github.com/zachasme/h3-pg/archive/refs/tags/v4.1.4.tar.gz"
+  sha256 "a7b5de78ec3874b284e7ef4ee7639c0c40e3ab49fa6eba0c53caef90a913994a"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
@@ -16,32 +15,30 @@ class H3Pg < Formula
   depends_on "cmake" => :build
   depends_on "bayandin/tap/neon-postgres"
 
-  patch :DATA
-
   def neon_postgres
     Formula["bayandin/tap/neon-postgres"]
   end
 
   def pg_versions
-    neon_postgres.pg_versions
+    neon_postgres.pg_versions(with: "v17")
   end
 
   def install
-    pg_versions.each do |v|
-      # Ref https://github.com/postgres/postgres/commit/b55f62abb2c2e07dfae99e19a2b3d7ca9e58dc1a
-      dlsuffix = (OS.linux? || "v14 v15".include?(v)) ? "so" : "dylib"
+    # h3-pg downloads h3 library
+    cmake_args = std_cmake_args.reject { |s| s.include? "trap_fetchcontent_provider.cmake" }
 
+    pg_versions.each do |v|
       mkdir buildpath/"build-#{v}"
 
       cd buildpath/"build-#{v}" do
-        system "cmake", "-DPostgreSQL_CONFIG=#{neon_postgres.pg_bin_for(v)}/pg_config", "..", *std_cmake_args
+        system "cmake", "-DPostgreSQL_CONFIG=#{neon_postgres.pg_bin_for(v)}/pg_config", "..", *cmake_args
         system "make"
         system "make", "install", "DESTDIR=#{buildpath}/stage-#{v}"
       end
 
       stage_dir = Pathname("stage-#{v}#{HOMEBREW_PREFIX}")
       mkdir_p lib/neon_postgres.name/v
-      mv Dir[stage_dir/"lib"/neon_postgres.name/v/"*.#{dlsuffix}"], lib/neon_postgres.name/v
+      mv Dir[stage_dir/"lib"/neon_postgres.name/v/"*.#{neon_postgres.dlsuffix(v)}"], lib/neon_postgres.name/v
 
       from_ext_dir = stage_dir/"share"/neon_postgres.name/v/"extension"
       to_ext_dir = share/neon_postgres.name/v/"extension"
@@ -74,22 +71,3 @@ class H3Pg < Formula
     end
   end
 end
-
-__END__
-diff --git a/cmake/AddPostgreSQLExtension.cmake b/cmake/AddPostgreSQLExtension.cmake
-index 655cbc2..e316ad3 100644
---- a/cmake/AddPostgreSQLExtension.cmake
-+++ b/cmake/AddPostgreSQLExtension.cmake
-@@ -44,6 +44,12 @@ function(PostgreSQL_add_extension LIBRARY_NAME)
-       PREFIX "" # Avoid lib* prefix on output file
-     )
-
-+    # Since Postgres 16, the shared library extension on macOS is `dylib`, not `so`.
-+    # Ref https://github.com/postgres/postgres/commit/b55f62abb2c2e07dfae99e19a2b3d7ca9e58dc1a
-+    if (APPLE AND ${PostgreSQL_VERSION_MAJOR} VERSION_GREATER_EQUAL "16")
-+      set_target_properties (${LIBRARY_NAME} PROPERTIES SUFFIX ".dylib")
-+    endif()
-+
-     # Install .so/.dll to pkglib-dir
-     install(
-       TARGETS ${LIBRARY_NAME}
