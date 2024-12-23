08e111613320a13bf3f93bce077f4c2db2f2b541
Alexander Bayandin <alexander@bayandin.dev>
2024-12-23 12:47:17 +0100

    neon-postgres 7381

--- Formula/neon-postgres.rb
+++ Formula/neon-postgres.rb
@@ -2,8 +2,8 @@ class NeonPostgres < Formula
   desc "Neon's fork of PostgreSQL"
   homepage "https://github.com/neondatabase/postgres"
   url "https://github.com/neondatabase/neon.git",
-    tag:      "release-7145",
-    revision: "1388bbae73cc714ed65d82240f6e0935eef805c6"
+    tag:      "release-7381",
+    revision: "671889b0e959a6520337ba3c56d645d6ff2ec1c1"
   license "Apache-2.0"
   head "https://github.com/neondatabase/neon.git", branch: "main"
 
