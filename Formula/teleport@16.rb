1f3c7dbd9a4bf07b76c9588430b9674c6f632744
Alexander Bayandin <alexander@neon.tech>
2025-05-23 15:30:18 +0200

    teleport@16 16.5.10
    Closes #285.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>

--- Formula/teleport@16.rb
+++ Formula/teleport@16.rb
@@ -1,8 +1,8 @@
 class TeleportAT16 < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://goteleport.com/"
-  url "https://github.com/gravitational/teleport/archive/refs/tags/v16.5.9.tar.gz"
-  sha256 "98a7ce295ea464d7c86162f0fb2d4fc52e62d9bd7108bdbe177f7d20c6628fd4"
+  url "https://github.com/gravitational/teleport/archive/refs/tags/v16.5.10.tar.gz"
+  sha256 "23815a22cc2b495c36cd8bc9eae0d438e9e306ca364a1f97898bb800e3858585"
   license all_of: ["AGPL-3.0-or-later", "Apache-2.0"]
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
