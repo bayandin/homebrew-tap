b289b2dfae79c75a1cdc22873907ea53d96ca654
Alexander Bayandin <alexander@bayandin.dev>
2025-08-02 23:45:08 +0000

    hypopg: update 1.4.2 bottle.

--- Formula/hypopg.rb
+++ Formula/hypopg.rb
@@ -12,10 +12,9 @@ class Hypopg < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/bayandin/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71912ba54e07431b16f1ee0ecccfed2bc7e5e44406d30e8873dac4b0d9a522b7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3af03fb671b6f9da7a15a816aecc0d0d71ddd221228925575c0b9a40dd314cb8"
-    sha256 cellar: :any_skip_relocation, ventura:       "b908fb783037fded25575bd86da547248161cdae1a61a37699303404901685c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b48615b47ba63f07716c0918bfcae4e0df8e4926ded197631cb2313d6280775a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b16748cb57867cbdaca8b42a633cc2c1cbf78bab3ea07d916784817f8f3130c0"
+    sha256 cellar: :any_skip_relocation, ventura:       "8ce5a9926a2ff3c5d45e8825e16984e12b0aaf3f35f4c38ad8b9478a46d8fe1d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e091a03e0d97bfb496c1268847c095d0488675182c0cd1aa51bdab252e38e14"
   end
 
   depends_on "bayandin/tap/neon-postgres"
